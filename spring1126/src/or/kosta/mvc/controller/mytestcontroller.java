6f5d9ea2e82b9a218ff7af523b76da3b974dcc12
KOSTA <KOSTA@KOSTA-PC>
2018-12-05 09:41:11 +0900

    tyest3

--- spring1126/src/or/kosta/mvc/controller/mytestcontroller.java
+++ spring1126/src/or/kosta/mvc/controller/mytestcontroller.java
@@ -1,14 +1,12 @@
 package or.kosta.mvc.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class mytestcontroller {
 
-	@RequestMapping(value="/test", RequestMethod="GET")
+	@RequestMapping(value="/test")
 	public String myView() {
 		
 		return "myview";
